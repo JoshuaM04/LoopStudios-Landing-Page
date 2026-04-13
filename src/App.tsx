5e8de3fd47d586b93e5387378242ee421250abcc
Viper <pruncle21@gmail.com>
2026-04-13 18:10:34 -0500

    Added a skeletal structure for the webpage.

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,60 @@
 export default function App() {
   return (
-    <div>
-      About
-      Careers
-      Events
-      Products
-      Support
+    <div className="parent-container min-h-screen [ grid grid-rows-3 justify-center items-center ] text-center">
+      <header>
+        <nav hidden={true} aria-hidden="true">
+          <ul>
+            <li>About</li>
+            <li>Careers</li>
+            <li>Events</li>
+            <li>Products</li>
+            <li>Support</li>
+          </ul>
+        </nav>
 
-      Immersive experiences that deliver
+        <h1>Immersive experiences that deliver</h1>
+      </header>
 
-      The leader in interactive VR
+      <main className="w-xs">
+        <figure>
+          <h2>The leader in interactive VR</h2>
+          <p>
+            Founded in 2011, Loopstudios has been producing world-class virtual reality
+            projects for some of the best companies around the globe. Our award-winning
+            creations have transformed businesses through digital experiences that bind
+            to their brand.
+          </p>
+        </figure>
 
-      Founded in 2011, Loopstudios has been producing world-class virtual reality 
-      projects for some of the best companies around the globe. Our award-winning 
-      creations have transformed businesses through digital experiences that bind 
-      to their brand.
+        <h2>Our creations</h2>
 
-      Our creations
+        <div hidden={true} aria-hidden="true">
+          Deep earth
+          Night arcade
+          Soccer team VR
+          The grid
+          From up above VR
+          Pocket borealis
+          The curiosity
+          Make it fisheye
+        </div>
+        <button className="uppercase">See all</button>
+      </main>
 
-      See all
+      <footer>
+        <h3>loopstudios</h3>
 
-      Deep earth
-      Night arcade
-      Soccer team VR
-      The grid
-      From up above VR
-      Pocket borealis
-      The curiosity
-      Make it fisheye
+        <nav>
+          <ul>
+            <li>About</li>
+            <li>Careers</li>
+            <li>Events</li>
+            <li>Products</li>
+            <li>Support</li>
+          </ul>
+        </nav>
 
-      About
-      Careers
-      Events
-      Products
-      Support
-
-      © 2021 Loopstudios. All rights reserved.
-
-      <footer className="attribution">
-        Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
-        Coded by <a href="#">Your Name Here</a>.
+        <p>© 2021 Loopstudios. All rights reserved.</p>
       </footer>
     </div>
   )

0d11f0661f7c532d83ce6e47bc56a7d3b55adaeb
Viper <pruncle21@gmail.com>
2026-04-15 13:58:15 -0500

    Added an onClick event type to the close-button in the mobile drop-down menu to control the display of the header and drop-down menu.

--- src/utils/HeaderComponent.tsx
+++ src/utils/HeaderComponent.tsx
@@ -2,22 +2,25 @@ import hamburgerIcon from "../assets/images/icon-hamburger.svg"
 import closeIcon from "../assets/images/icon-close.svg";
 import { useState } from 'react';
 
-function handleMenu(setisHeaderHidden: Function, setIsMenuHidden: Function, isHeaderHidden: boolean, isMenuHidden: boolean) {
+function handleMenu(setIsHeaderHidden: Function, setIsMenuHidden: Function, isHeaderHidden: boolean, isMenuHidden: boolean) {
     if (isHeaderHidden === false && isMenuHidden === true) {
-        setisHeaderHidden(true);
+        setIsHeaderHidden(true);
         setIsMenuHidden(false);
-    } 
+    } else {
+        setIsHeaderHidden(false);
+        setIsMenuHidden(true);
+    }
 }
 
 export function MobileHeader() {
-    const [isHeaderHidden, setisHeaderHidden] = useState(false);
+    const [isHeaderHidden, setIsHeaderHidden] = useState(false);
     const [isMenuHidden, setIsMenuHidden] = useState(true);
 
     return (
         <header className="[ grid ] [ p-[20px] ] [ min-h-[650px] w-screen ] [ bg-[url(assets/images/mobile/image-hero.jpg)] bg-center bg-cover bg-no-repeat ]">
             <section hidden={isHeaderHidden} aria-hidden={isHeaderHidden} className="[ flex flex-row items-center justify-between ] [ h-min ]">
                 <p className="[ text-white text-left ] [ font-medium ]">loopstudios</p>
-                <button onClick={() => handleMenu(setisHeaderHidden, setIsMenuHidden, isHeaderHidden, isMenuHidden)} className="[ rounded-sm p-1 ] [ hover:cursor-pointer hover:bg-indigo-500 ]" aria-label="drop-down menu"><img src={hamburgerIcon} aria-hidden="true"/></button>
+                <button onClick={() => handleMenu(setIsHeaderHidden, setIsMenuHidden, isHeaderHidden, isMenuHidden)} className="[ rounded-sm p-1 ] [ hover:cursor-pointer hover:bg-indigo-500 ]" aria-label="drop-down menu"><img src={hamburgerIcon} aria-hidden="true"/></button>
             </section>
 
             <h1 hidden={isHeaderHidden} aria-hidden={isHeaderHidden} className="[ self-start ] [ uppercase text-4xl text-left text-slate-300 tracking-wider font-light ] [ p-5 ] [ border-2 border-slate-300 ]">Immersive experiences that deliver</h1>
@@ -25,7 +28,7 @@ export function MobileHeader() {
             <section className="grid grid-cols 1 auto-rows-min gap-35 m-[-20px] p-5 bg-black text-white" hidden={isMenuHidden} aria-hidden={isMenuHidden}>
                 <div className="flex justify-between self-start">
                     <p className="text-2xl font-bold">loopstudios</p>
-                    <button aria-label="close-menu"><img src={closeIcon} aria-hidden="true" /></button>
+                    <button onClick={() => handleMenu(setIsHeaderHidden, setIsMenuHidden, isHeaderHidden, isMenuHidden)} aria-label="close-menu"><img src={closeIcon} aria-hidden="true" /></button>
                 </div>
 
                 <nav className="text-3xl/15 text-left font-light uppercase">

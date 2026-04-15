a88bf3897a9bdd756ebdf7d30f14c0f533dc1d7e
Viper <pruncle21@gmail.com>
2026-04-15 02:10:03 -0500

    Refined the destructuring for the state variables in HeaderComponent, so they are more clear to read over.

--- src/utils/HeaderComponent.tsx
+++ src/utils/HeaderComponent.tsx
@@ -1,18 +1,23 @@
 import hamburgerIcon from "../assets/images/icon-hamburger.svg"
 import { useState } from 'react';
 
+function handleMenu(setisHeaderHidden: Function, setIsMenuHidden: Function) {
+    setisHeaderHidden(false);
+    setIsMenuHidden(true);
+}
+
 export function MobileHeader() {
-    const [activeHeader, setActiveHeader] = useState(true);
-    const [activeMenu, setActiveMenu] = useState(false);
+    const [isHeaderHidden, setisHeaderHidden] = useState(false);
+    const [isMenuHidden, setIsMenuHidden] = useState(true);
 
     return (
         <header className="[ grid ] [ p-5 ] [ min-h-[650px] w-screen ] [ bg-[url(assets/images/mobile/image-hero.jpg)] bg-center bg-cover bg-no-repeat ]">
-            <section hidden={!(activeHeader)} aria-hidden={!(activeHeader)} className="[ flex flex-row items-center justify-between ] [ h-min ]">
+            <section hidden={isHeaderHidden} aria-hidden={isHeaderHidden} className="[ flex flex-row items-center justify-between ] [ h-min ]">
                 <p className="[ text-white text-left ] [ font-medium ]">loopstudios</p>
-                <button className="[ rounded-sm p-1 ] [ hover:cursor-pointer hover:bg-indigo-500 ]" aria-label="drop-down menu"><img src={hamburgerIcon} /></button>
+                <button onClick={() => handleMenu(setisHeaderHidden, setIsMenuHidden)} className="[ rounded-sm p-1 ] [ hover:cursor-pointer hover:bg-indigo-500 ]" aria-label="drop-down menu"><img src={hamburgerIcon} /></button>
             </section>
 
-            <nav hidden={!(activeMenu)} aria-hidden={!(activeMenu)}>
+            <nav hidden={isMenuHidden} aria-hidden={isMenuHidden}>
                 <ul>
                     <li>About</li>
                     <li>Careers</li>
@@ -22,7 +27,7 @@ export function MobileHeader() {
                 </ul>
             </nav>
 
-            <h1 hidden={!(activeHeader)} aria-hidden={!(activeHeader)} className="[ self-start ] [ uppercase text-4xl text-left text-slate-300 tracking-wider font-light ] [ p-5 ] [ border-2 border-slate-300 ]">Immersive experiences that deliver</h1>
+            <h1 hidden={isHeaderHidden} aria-hidden={isHeaderHidden} className="[ self-start ] [ uppercase text-4xl text-left text-slate-300 tracking-wider font-light ] [ p-5 ] [ border-2 border-slate-300 ]">Immersive experiences that deliver</h1>
         </header>
     );
 }

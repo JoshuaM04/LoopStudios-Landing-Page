cabfd2226ea75e0b27bae2063eeda804724db8e4
Viper <pruncle21@gmail.com>
2026-04-15 13:53:38 -0500

    Added the drop-down menu on mobile devices. Added styling and interactivity and adjusted the layout of the content in the drop-down menu on mobile devices.

--- src/utils/HeaderComponent.tsx
+++ src/utils/HeaderComponent.tsx
@@ -1,4 +1,5 @@
 import hamburgerIcon from "../assets/images/icon-hamburger.svg"
+import closeIcon from "../assets/images/icon-close.svg";
 import { useState } from 'react';
 
 function handleMenu(setisHeaderHidden: Function, setIsMenuHidden: Function, isHeaderHidden: boolean, isMenuHidden: boolean) {
@@ -13,23 +14,30 @@ export function MobileHeader() {
     const [isMenuHidden, setIsMenuHidden] = useState(true);
 
     return (
-        <header className="[ grid ] [ p-5 ] [ min-h-[650px] w-screen ] [ bg-[url(assets/images/mobile/image-hero.jpg)] bg-center bg-cover bg-no-repeat ]">
+        <header className="[ grid ] [ p-[20px] ] [ min-h-[650px] w-screen ] [ bg-[url(assets/images/mobile/image-hero.jpg)] bg-center bg-cover bg-no-repeat ]">
             <section hidden={isHeaderHidden} aria-hidden={isHeaderHidden} className="[ flex flex-row items-center justify-between ] [ h-min ]">
                 <p className="[ text-white text-left ] [ font-medium ]">loopstudios</p>
-                <button onClick={() => handleMenu(setisHeaderHidden, setIsMenuHidden, isHeaderHidden, isMenuHidden)} className="[ rounded-sm p-1 ] [ hover:cursor-pointer hover:bg-indigo-500 ]" aria-label="drop-down menu"><img src={hamburgerIcon} /></button>
+                <button onClick={() => handleMenu(setisHeaderHidden, setIsMenuHidden, isHeaderHidden, isMenuHidden)} className="[ rounded-sm p-1 ] [ hover:cursor-pointer hover:bg-indigo-500 ]" aria-label="drop-down menu"><img src={hamburgerIcon} aria-hidden="true"/></button>
             </section>
 
-            <nav hidden={isMenuHidden} aria-hidden={isMenuHidden}>
-                <ul>
-                    <li>About</li>
-                    <li>Careers</li>
-                    <li>Events</li>
-                    <li>Products</li>
-                    <li>Support</li>
-                </ul>
-            </nav>
-
             <h1 hidden={isHeaderHidden} aria-hidden={isHeaderHidden} className="[ self-start ] [ uppercase text-4xl text-left text-slate-300 tracking-wider font-light ] [ p-5 ] [ border-2 border-slate-300 ]">Immersive experiences that deliver</h1>
+
+            <section className="grid grid-cols 1 auto-rows-min gap-35 m-[-20px] p-5 bg-black text-white" hidden={isMenuHidden} aria-hidden={isMenuHidden}>
+                <div className="flex justify-between self-start">
+                    <p className="text-2xl font-bold">loopstudios</p>
+                    <button aria-label="close-menu"><img src={closeIcon} aria-hidden="true" /></button>
+                </div>
+
+                <nav className="text-3xl/15 text-left font-light uppercase">
+                    <ul>
+                        <li>About</li>
+                        <li>Careers</li>
+                        <li>Events</li>
+                        <li>Products</li>
+                        <li>Support</li>
+                    </ul>
+                </nav>
+            </section>
         </header>
     );
 }

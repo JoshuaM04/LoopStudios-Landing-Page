d50f20e85d3b768d194da14fd00a01ee8192b57e
Viper <pruncle21@gmail.com>
2026-04-14 19:25:58 -0500

    Temporarily removed the state variables from the hidden and aria-hidden attributes to entirely avoid the issue with setter functions not being used.

--- src/App.tsx
+++ src/App.tsx
@@ -40,23 +40,26 @@ export default function App() {
   const boxes = ["DEEP EARTH", "NIGHT ARCADE", "SOCCER TEAM VR", "THE GRID", "FROM UP ABOVE VR", "POCKET BOREALIS", "THE CURIOSITY", "MAKE IT FISHEYE"];
   const bgImage = ["bg-[url(assets/images/mobile/image-deep-earth.jpg)]", "bg-[url(assets/images/mobile/image-night-arcade.jpg)]", "bg-[url(assets/images/mobile/image-soccer-team.jpg)]", "bg-[url(assets/images/mobile/image-grid.jpg)]", 
                   "bg-[url(assets/images/mobile/image-from-above.jpg)]", "bg-[url(assets/images/mobile/image-pocket-borealis.jpg)]", "bg-[url(assets/images/mobile/image-curiosity.jpg)]", "bg-[url(assets/images/mobile/image-fisheye.jpg)]"];
-  const [activeHeader, setActiveHeader] = useState(true);
-  const [activeMenu, setActiveMenu] = useState(false);
+  // const [activeHeader, setActiveHeader] = useState(true);
+  // const [activeMenu, setActiveMenu] = useState(false);
   let count = -1;
-  
-  setActiveHeader(true);
-  setActiveMenu(false);
-  
+
+  // This will take the place of the header element in the App functional component. The header element should be moved within the return statement of the MobileHeader functional component
+  /* <MobileHeader 
+        activeHeader={activeHeader}
+        activeMenu={activeMenu}
+  /> */
+
   return (
     <div className="[ parent-container ] [ flex flex-col justify-between items-center gap-20 ] [ text-center ] [ p-[0_20px_0_20px] ] [ min-h-screen w-screen ]">
 
       <header className="[ grid ] [ p-5 ] [ min-h-[650px] w-screen ] [ bg-[url(assets/images/mobile/image-hero.jpg)] bg-center bg-cover bg-no-repeat ]">
-        <section hidden={!(activeHeader)} aria-hidden={!(activeHeader)} className="[ flex flex-row items-center justify-between ] [ h-min ]">
+        <section hidden={false} aria-hidden={false} className="[ flex flex-row items-center justify-between ] [ h-min ]">
           <p className="[ text-white text-left ] [ font-medium ]">loopstudios</p>
           <button className="[ rounded-sm p-1 ] [ hover:cursor-pointer hover:bg-indigo-500 ]" aria-label="drop-down menu"><img src={hamburgerIcon} /></button>
         </section>
 
-        <nav hidden={!(activeMenu)} aria-hidden={!(activeMenu)}>
+        <nav hidden={true} aria-hidden={true}>
             <ul>
               <li>About</li>
               <li>Careers</li>
@@ -66,7 +69,7 @@ export default function App() {
             </ul>
         </nav>
 
-        <h1 hidden={!(activeHeader)} aria-hidden={!(activeHeader)} className="[ self-start ] [ uppercase text-4xl text-left text-slate-300 tracking-wider font-light ] [ p-5 ] [ border-2 border-slate-300 ]">Immersive experiences that deliver</h1>
+        <h1 hidden={false} aria-hidden={false} className="[ self-start ] [ uppercase text-4xl text-left text-slate-300 tracking-wider font-light ] [ p-5 ] [ border-2 border-slate-300 ]">Immersive experiences that deliver</h1>
       </header>
 
       <main className="flex flex-col gap-20">

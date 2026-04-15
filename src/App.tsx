eb19632b424ea7598400a9cd175ec9390841768b
Viper <pruncle21@gmail.com>
2026-04-14 19:17:46 -0500

    Made temporary adjustments to the content organization with a functional component for the header.

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,24 @@ import facebookIcon from "./assets/images/icon-facebook.svg";
 import twitterIcon from "./assets/images/icon-twitter.svg";
 import pinterestIcon from "./assets/images/icon-pinterest.svg";
 import instagramIcon from "./assets/images/icon-instagram.svg";
+import { useState } from 'react';
 
-export default function App() {
-  const boxes = ["DEEP EARTH", "NIGHT ARCADE", "SOCCER TEAM VR", "THE GRID", "FROM UP ABOVE VR", "POCKET BOREALIS", "THE CURIOSITY", "MAKE IT FISHEYE"];
-  const bgImage = ["bg-[url(assets/images/mobile/image-deep-earth.jpg)]", "bg-[url(assets/images/mobile/image-night-arcade.jpg)]", "bg-[url(assets/images/mobile/image-soccer-team.jpg)]", "bg-[url(assets/images/mobile/image-grid.jpg)]", 
-                  "bg-[url(assets/images/mobile/image-from-above.jpg)]", "bg-[url(assets/images/mobile/image-pocket-borealis.jpg)]", "bg-[url(assets/images/mobile/image-curiosity.jpg)]", "bg-[url(assets/images/mobile/image-fisheye.jpg)]"];
-  let count = -1;
+/* interface MobileHeader {
+  activeHeader: boolean;
+  activeMenu: boolean;
+} */
+
+function MobileHeader(activeHeader: boolean, activeMenu: boolean) {
+  console.log("Active Header: " + activeHeader);
 
   return (
-    <div className="[ parent-container ] [ flex flex-col justify-between items-center gap-20 ] [ text-center ] [ p-[0_20px_0_20px] ] [ min-h-screen w-screen ]">
-      <header className="[ grid ] [ p-5 ] [ min-h-[650px] w-screen ] [ bg-[url(assets/images/mobile/image-hero.jpg)] bg-center bg-cover bg-no-repeat ]">
-        <section className="[ flex flex-row items-center justify-between ] [ h-min ]">
+    <header className="[ grid ] [ p-5 ] [ min-h-[650px] w-screen ] [ bg-[url(assets/images/mobile/image-hero.jpg)] bg-center bg-cover bg-no-repeat ]">
+        <section hidden={!(activeHeader)} aria-hidden={!(activeHeader)} className="[ flex flex-row items-center justify-between ] [ h-min ]">
           <p className="[ text-white text-left ] [ font-medium ]">loopstudios</p>
           <button className="[ rounded-sm p-1 ] [ hover:cursor-pointer hover:bg-indigo-500 ]" aria-label="drop-down menu"><img src={hamburgerIcon} /></button>
+        </section>
 
-          <nav hidden={true} aria-hidden="true">
+        <nav hidden={!(activeMenu)} aria-hidden={!(activeMenu)}>
             <ul>
               <li>About</li>
               <li>Careers</li>
@@ -26,11 +29,28 @@ export default function App() {
               <li>Products</li>
               <li>Support</li>
             </ul>
-          </nav>
-        </section>
+        </nav>
 
-        <h1 className="[ self-start ] [ uppercase text-4xl text-left text-slate-300 tracking-wider font-light ] [ p-5 ] [ border-2 border-slate-300 ]">Immersive experiences that deliver</h1>
+        <h1 hidden={!(activeHeader)} aria-hidden={!(activeHeader)} className="[ self-start ] [ uppercase text-4xl text-left text-slate-300 tracking-wider font-light ] [ p-5 ] [ border-2 border-slate-300 ]">Immersive experiences that deliver</h1>
       </header>
+  );
+}
+
+export default function App() {
+  const boxes = ["DEEP EARTH", "NIGHT ARCADE", "SOCCER TEAM VR", "THE GRID", "FROM UP ABOVE VR", "POCKET BOREALIS", "THE CURIOSITY", "MAKE IT FISHEYE"];
+  const bgImage = ["bg-[url(assets/images/mobile/image-deep-earth.jpg)]", "bg-[url(assets/images/mobile/image-night-arcade.jpg)]", "bg-[url(assets/images/mobile/image-soccer-team.jpg)]", "bg-[url(assets/images/mobile/image-grid.jpg)]", 
+                  "bg-[url(assets/images/mobile/image-from-above.jpg)]", "bg-[url(assets/images/mobile/image-pocket-borealis.jpg)]", "bg-[url(assets/images/mobile/image-curiosity.jpg)]", "bg-[url(assets/images/mobile/image-fisheye.jpg)]"];
+  const [activeHeader, setActiveHeader] = useState(true);
+  const [activeMenu, setActiveMenu] = useState(true);
+  let count = -1;
+
+  return (
+    <div className="[ parent-container ] [ flex flex-col justify-between items-center gap-20 ] [ text-center ] [ p-[0_20px_0_20px] ] [ min-h-screen w-screen ]">
+      
+      <MobileHeader 
+        activeHeader={activeHeader}
+        activeMenu={activeMenu}
+      />
 
       <main className="flex flex-col gap-20">
         <section className="flex flex-col gap-5">

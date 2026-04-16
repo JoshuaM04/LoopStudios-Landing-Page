bd31ff64a70a985b8e1e39f8d07dfe02823a0f5d
Viper <pruncle21@gmail.com>
2026-04-15 23:11:40 -0500

    Made adjustments to the typeWriter function.

--- src/utils/HeaderComponent.tsx
+++ src/utils/HeaderComponent.tsx
@@ -18,8 +18,9 @@ export function MobileHeader() {
     const [heading, setHeading] = useState("");
     const headingVal = "Immersive experiences that deliver";
     let index = 0;
-    const speed = 400;
+    const speed = 500;
 
+    // Any way to make this slower without chunks of text being rendered at each interval rather than a single character?
     useEffect(() => {
         typeWriter();
 

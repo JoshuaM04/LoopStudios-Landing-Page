07a11727b4d8bba3661430ee0c0df034dc6b2f7b
Viper <pruncle21@gmail.com>
2026-04-15 21:33:29 -0500

    Added 'aria-controls', 'aria-expanded', and an id to the respective elements in the HeaderComponent.tsx file. This is to ensure anyone using web readers or assistive technology is able to be informed whether the menu is toggled, what controls the menu, and the association between a button as well as the menu drop-down.

--- src/utils/HeaderComponent.tsx
+++ src/utils/HeaderComponent.tsx
@@ -21,17 +21,17 @@ export function MobileHeader() {
             <section hidden={isHeaderHidden} aria-hidden={isHeaderHidden} className="[ flex flex-row items-center justify-between ] [ h-min ]">
                 <p className="[ text-white text-left ] [ font-medium ]">loopstudios</p>
                 <button onClick={() => handleMenu(setIsHeaderHidden, setIsMenuHidden, isHeaderHidden, isMenuHidden)} 
-                    className="[ rounded-sm p-1 ] [ hover:cursor-pointer hover:bg-black ] [ transition delay-150 duration-300 ease-in-out hover-translate-y-1 hover:scale-110]" aria-label="drop-down menu">
+                    className="[ rounded-sm p-1 ] [ hover:cursor-pointer hover:bg-black ] [ transition delay-150 duration-300 ease-in-out hover-translate-y-1 hover:scale-110]" aria-label="drop-down menu" aria-controls="HeaderMenu">
                     <img src={hamburgerIcon} aria-hidden="true" />
                 </button>
             </section>
 
             <h1 hidden={isHeaderHidden} aria-hidden={isHeaderHidden} className="[ self-start ] [ uppercase text-4xl text-left text-slate-300 tracking-wider font-light ] [ p-5 ] [ border-2 border-slate-300 ]">Immersive experiences that deliver</h1>
 
-            <section className="grid grid-cols 1 auto-rows-min gap-35 m-[-20px] p-5 bg-black text-white" hidden={isMenuHidden} aria-hidden={isMenuHidden}>
+            <section id="HeaderMenu" className="grid grid-cols 1 auto-rows-min gap-35 m-[-20px] p-5 bg-black text-white" hidden={isMenuHidden} aria-hidden={isMenuHidden} aria-expanded={isMenuHidden}>
                 <div className="flex justify-between self-start">
                     <p className="text-2xl font-bold">loopstudios</p>
-                    <button onClick={() => handleMenu(setIsHeaderHidden, setIsMenuHidden, isHeaderHidden, isMenuHidden)} aria-label="close-menu"><img src={closeIcon} aria-hidden="true" /></button>
+                    <button onClick={() => handleMenu(setIsHeaderHidden, setIsMenuHidden, isHeaderHidden, isMenuHidden)} aria-label="close-menu"><img src={closeIcon} aria-hidden="true" aria-controls="HeaderMenu"/></button>
                 </div>
 
                 <nav className="text-3xl/15 text-left font-light uppercase select-none">

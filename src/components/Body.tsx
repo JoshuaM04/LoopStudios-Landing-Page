0e43de8cb253a2b656289dc6ee7164be2e08fb97
Viper <pruncle21@gmail.com>
2026-04-16 19:04:45 -0500

    Added some breakpoints to the body of the webpage and overlayed the 'The Leader in Interactive VR' text section over its related image on desktop resolutions.

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -2,42 +2,55 @@ import interactiveImg from "../assets/images/mobile/image-interactive.jpg";
 
 export default function Body() {
     const boxes = ["DEEP EARTH", "NIGHT ARCADE", "SOCCER TEAM VR", "THE GRID", "FROM UP ABOVE VR", "POCKET BOREALIS", "THE CURIOSITY", "MAKE IT FISHEYE"];
-    const bgImage = ["bg-[url(assets/images/mobile/image-deep-earth.jpg)]", "bg-[url(assets/images/mobile/image-night-arcade.jpg)]", "bg-[url(assets/images/mobile/image-soccer-team.jpg)]", "bg-[url(assets/images/mobile/image-grid.jpg)]", 
-                  "bg-[url(assets/images/mobile/image-from-above.jpg)]", "bg-[url(assets/images/mobile/image-pocket-borealis.jpg)]", "bg-[url(assets/images/mobile/image-curiosity.jpg)]", "bg-[url(assets/images/mobile/image-fisheye.jpg)]"];
+    const bgImageMobile = ["bg-[url(assets/images/mobile/image-deep-earth.jpg)]", "bg-[url(assets/images/mobile/image-night-arcade.jpg)]", "bg-[url(assets/images/mobile/image-soccer-team.jpg)]", "bg-[url(assets/images/mobile/image-grid.jpg)]", 
+                            "bg-[url(assets/images/mobile/image-from-above.jpg)]", "bg-[url(assets/images/mobile/image-pocket-borealis.jpg)]", "bg-[url(assets/images/mobile/image-curiosity.jpg)]", "bg-[url(assets/images/mobile/image-fisheye.jpg)]"];
+    const bgImageDesktop = ["bg-[url(assets/images/desktop/image-deep-earth.jpg)]", "bg-[url(assets/images/desktop/image-night-arcade.jpg)]", "bg-[url(assets/images/desktop/image-soccer-team.jpg)]", "bg-[url(assets/images/desktop/image-grid.jpg)]", 
+                            "bg-[url(assets/images/desktop/image-from-above.jpg)]", "bg-[url(assets/images/desktop/image-pocket-borealis.jpg)]", "bg-[url(assets/images/desktop/image-curiosity.jpg)]", "bg-[url(assets/images/desktop/image-fisheye.jpg)]"];
     let count = -1;
     
     return (
-        <main className="flex flex-col gap-20">
-            <section className="flex flex-col gap-5">
-                <img src={interactiveImg} alt="A man in a blue t-shirt wearing a VR-headset and is in a T-Rex like posture with his arms." />
+        <main className="grid grid-cols-1 gap-20 w-full md:gap-50">
+            <section className="flex flex-col gap-5 items-center md:items-start w-full md:relative md:max-w-[800px] md:place-self-center">
+                <img src={interactiveImg} alt="A man in a blue t-shirt wearing a VR-headset and is in a T-Rex like posture with his arms." className="md:h-75 md:w-100" />
 
-                <h2 className="text-3xl tracking-wider font-light capitalize">The leader in interactive VR</h2>
-
-                <p className="text-gray-500 font-medium">
-                Founded in 2011, Loopstudios has been producing world-class virtual reality
-                projects for some of the best companies around the globe. Our award-winning
-                creations have transformed businesses through digital experiences that bind
-                to their brand.
-                </p>
+                <div className="flex flex-col md:text-left md:gap-5 md:w-90 md:absolute md:top-30 md:right-30 md:p-10 bg-white">
+                    <h2 className="text-3xl tracking-wider font-light capitalize md:text-4xl">The leader in interactive VR</h2>
+                    <p className="text-gray-500 font-medium md:text-xs">
+                        Founded in 2011, Loopstudios has been producing world-class virtual reality
+                        projects for some of the best companies around the globe. Our award-winning
+                        creations have transformed businesses through digital experiences that bind
+                        to their brand.
+                    </p>
+                </div>
             </section>
 
-            <section className="flex flex-col gap-10 items-center">
-                <h2 className="text-3xl tracking-wider font-light uppercase">Our creations</h2>
+            <section className="grid grid-cols-1 gap-10">
+                <div className="flex justify-center md:justify-between">
+                    <h2 className="text-3xl tracking-wider font-light uppercase md:place-self-start">Our creations</h2>
+                    <button className="uppercase tracking-wider border-2 w-30 p-1 transition delay-150 duration-300 ease-in-out hover:scale-115 active:scale-115 select-none hover:cursor-pointer place-self-center max-md:hidden max-md:aria-hidden">See all</button>
+                </div>
 
-                <div className="grid grid-rows-8 gap-5 w-full">
+                <div className="grid grid-rows-8 gap-5 w-full md:grid-rows-2 md:grid-cols-4">
                 {
                     boxes.map((item, index) => {
                     count++;
                     return (
-                        <section key={index} className={`[ text-white text-xl text-bottom font-light ] [ transition delay-150 duration-300 ease-in-out hover:scale-105 active:scale-105 hover:cursor-pointer ] [ ${bgImage[count]} bg-center bg-cover bg-no-repeat ] [ p-5 ] [ min-h-30 ] [ relative ]`}>
-                        <h3 className="absolute bottom-5 select-none">{item}</h3>
-                        </section>
+                        <div>
+                            <section key={index} 
+                                     className={`[ text-white text-xl text-bottom font-light ] [ transition delay-150 duration-300 ease-in-out hover:scale-105 active:scale-105 hover:cursor-pointer ] [ ${bgImageMobile[count]} bg-center bg-cover bg-no-repeat ] [ p-5 ] [ min-h-30 ] [ relative ] md:hidden md-aria-hidden`}>
+                                <h3 className="absolute bottom-5 select-none">{item}</h3>
+                            </section>
+                            <section key={index} 
+                                     className={`[ text-white text-xl text-bottom font-light ] [ transition delay-150 duration-300 ease-in-out hover:scale-105 active:scale-105 hover:cursor-pointer ] [ ${bgImageDesktop[count]} bg-center bg-contain bg-no-repeat ] [ p-5 ] [ min-h-80 ] [ relative ] max-md:hidden max-md:aria-hidden`}>
+                                <h3 className="absolute bottom-5 select-none">{item}</h3>
+                            </section>
+                        </div>   
                     )
                     })
                 }
                 </div>
 
-                <button className="uppercase tracking-wider border-2 w-30 p-1 transition delay-150 duration-300 ease-in-out hover:scale-115 active:scale-115 select-none hover:cursor-pointer">See all</button>
+                <button className="uppercase tracking-wider border-2 w-30 p-1 transition delay-150 duration-300 ease-in-out hover:scale-115 active:scale-115 select-none hover:cursor-pointer place-self-center md:hidden md:aria-hidden">See all</button>
             </section>
         </main>
     )

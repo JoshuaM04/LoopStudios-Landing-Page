038dcd1ecb78b110583df1a1ae37f5c7461534c4
Viper <pruncle21@gmail.com>
2026-04-19 16:19:51 -0500

    Modularized the Header.tsx file so that there are two different functional components within the main Header() functional component that is then exported as a named export. The functional components represent mobile and desktop layouts respectively. This is to keep things organized and seperate breakpoints into their respective sections.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { MobileHeader } from "./components/Header.tsx";
+import Header from "./components/Header.tsx";
 import BodyComponent  from "./components/Body.tsx";
 import Footer from "./components/Footer.tsx";
 
 export default function App() {
   return (
     <div className="[ parent-container ] [ flex flex-col justify-between items-center gap-20 2xl:gap-100 ] [ text-center ] [ p-[0_20px_0_20px] ] [ min-h-screen max-w-screen box-border] font-alata">
-      <MobileHeader />
+      <Header />
       <BodyComponent />
       <Footer />
     </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,79 @@ import hamburgerIcon from "../assets/images/icon-hamburger.svg"
 import closeIcon from "../assets/images/icon-close.svg";
 import { useState, useEffect } from 'react';
 
+interface MobileProps {
+    isHeaderHidden: boolean;
+    setIsHeaderHidden: Function;
+    isMenuHidden: boolean;
+    setIsMenuHidden: Function;
+    heading: string;
+}
+
+interface DesktopProps {
+    heading: string;
+}
+
+function MobileHeader({isHeaderHidden, setIsHeaderHidden, isMenuHidden, setIsMenuHidden, heading}: MobileProps) {
+    return (
+        <div className="xl:hidden min-h-full">
+            <section id="DefaultHeader" className="grid grid-cols-1 gap-50" hidden={isHeaderHidden} aria-hidden={isHeaderHidden}>
+                <div className="flex items-center justify-between">
+                    <p className="[ text-3xl text-white text-left ] [ font-medium ]">loopstudios</p>
+                    
+                    <button onClick={() => handleMenu(setIsHeaderHidden, setIsMenuHidden, isHeaderHidden, isMenuHidden)} className="[ rounded-sm p-1 ] [ hover:cursor-pointer transition delay-50 duration-150 ease-in-out hover:scale-120 active:scale-120 ] xl:hidden" aria-label="drop-down menu" aria-controls="HeaderMenu">
+                        <img src={hamburgerIcon} className="pointer-events-none" aria-hidden="true" />
+                    </button>
+                </div>
+
+                <h1 className="[ self-start ] [ uppercase text-5xl md:text-7xl text-left text-white tracking-widest font-light ] [ p-5 ] [ border-2 border-white ] [ min-h-23 md:w-150 ] animate-fade-in-textBox" hidden={isHeaderHidden} aria-hidden={isHeaderHidden}>
+                    {heading}
+                </h1>
+            </section>
+
+            <section id="HeaderMenu" className="grid grid-cols 1 auto-rows-min gap-35 -m-5 md:m-[-40px_-50px_-40px_-50px] md:p-[40px_50px_40px_50px] p-10 bg-black text-white animate-fade-in-dropDownMenu h-163" hidden={isMenuHidden} aria-hidden={isMenuHidden} aria-expanded={!(isMenuHidden)}>
+                <div className="flex justify-between self-start">
+                    <p className="text-3xl font-bold">loopstudios</p>
+                    <button onClick={() => handleMenu(setIsHeaderHidden, setIsMenuHidden, isHeaderHidden, isMenuHidden)} className="hover:cursor-pointer transition delay-50 duration-150 ease-in-out hover:scale-120 active:scale-120" aria-label="close-menu">
+                        <img src={closeIcon} className="pointer-events-none" aria-hidden="true" aria-controls="HeaderMenu"/>
+                    </button>
+                </div>
+
+                <nav className="text-3xl/15 text-left font-light uppercase select-none">
+                    <ul>
+                        <li className="transition delay-50 duration-150 ease-in-out hover:scale-102 hover:text-purple-500 active:scale-102 active:text-purple-500 animate-fade-in-about"><a href="#">About</a></li>
+                        <li className="transition delay-50 duration-150 ease-in-out hover:scale-102 hover:text-purple-500 active:scale-102 active:text-purple-500 animate-fade-in-careers"><a href="#">Careers</a></li>
+                        <li className="transition delay-50 duration-150 ease-in-out hover:scale-102 hover:text-purple-500 active:scale-102 active:text-purple-500 animate-fade-in-events"><a href="#">Events</a></li>
+                        <li className="transition delay-50 duration-150 ease-in-out hover:scale-102 hover:text-purple-500 active:scale-102 active:text-purple-500 animate-fade-in-products"><a href="#">Products</a></li>
+                        <li className="transition delay-50 duration-150 ease-in-out hover:scale-102 hover:text-purple-500 active:scale-102 active:text-purple-500 animate-fade-in-support"><a href="#">Support</a></li>
+                    </ul>
+                </nav>
+            </section>
+        </div>
+    )
+}
+
+function DesktopHeader({heading}: DesktopProps) {
+    return (
+        <div className="max-xl:hidden">
+            <section className="[ flex flex-row items-center justify-between ] [ h-min ]">
+                <p className="[ xl:text-5xl text-white text-left ] [ font-medium ]">loopstudios</p>
+
+                <div className="text-white inline-flex gap-10 text-xl">
+                    <a href="#" className="transition delay-50 duration-150 ease-in-out hover:scale-120 hover:border-b active:scale-120 active:border-b">About</a>
+                    <a href="#" className="transition delay-50 duration-150 ease-in-out hover:scale-120 hover:border-b active:scale-120 active:border-b">Careers</a>
+                    <a href="#" className="transition delay-50 duration-150 ease-in-out hover:scale-120 hover:border-b active:scale-120 active:border-b">Events</a>
+                    <a href="#" className="transition delay-50 duration-150 ease-in-out hover:scale-120 hover:border-b active:scale-120 active:border-b">Products</a>
+                    <a href="#" className="transition delay-50 duration-150 ease-in-out hover:scale-120 hover:border-b active:scale-120 active:border-b">Support</a>
+                </div>
+
+                <h1 className="[ self-start ] [ uppercase text-5xl md:text-7xl xl:text-9xl text-left text-white tracking-widest font-light ] [ p-5 ] [ border-2 border-white ] [ min-h-21.25 md:w-150 xl:w-300 xl:min-h-45 ] animate-fade-in-textBox place-self-center">
+                    {heading}
+                </h1>
+            </section>
+        </div>
+    )
+}
+
 function handleMenu(setIsHeaderHidden: Function, setIsMenuHidden: Function, isHeaderHidden: boolean, isMenuHidden: boolean) {
     if (isHeaderHidden === false && isMenuHidden === true) {
         setIsHeaderHidden(true);
@@ -12,7 +85,7 @@ function handleMenu(setIsHeaderHidden: Function, setIsMenuHidden: Function, isHe
     }
 }
 
-export function MobileHeader() {
+export default function Header() {
     const [isHeaderHidden, setIsHeaderHidden] = useState(false);
     const [isMenuHidden, setIsMenuHidden] = useState(true);
     const [heading, setHeading] = useState("");
@@ -33,46 +106,18 @@ export function MobileHeader() {
     }, [])
 
     return (
-        <header className="[ grid ] [ p-5 md:p-[40px_50px_40px_50px] ] [ min-h-162.5 w-screen xl:min-h-350 ] [ bg-[url(assets/images/mobile/image-hero.jpg)] bg-center bg-cover bg-no-repeat md:bg-[url(assets/images/desktop/image-hero.jpg)] ] select-none">
-            <section hidden={isHeaderHidden} aria-hidden={isHeaderHidden} className="[ flex flex-row items-center justify-between ] [ h-min ]">
-                <p className="[ text-3xl xl:text-5xl text-white text-left ] [ font-medium ]">loopstudios</p>
-                <button onClick={() => handleMenu(setIsHeaderHidden, setIsMenuHidden, isHeaderHidden, isMenuHidden)} 
-                    className="[ rounded-sm p-1 ] [ hover:cursor-pointer transition delay-50 duration-150 ease-in-out hover:scale-120 active:scale-120 ] xl:hidden" aria-label="drop-down menu" aria-controls="HeaderMenu">
-                    <img src={hamburgerIcon} className="pointer-events-none" aria-hidden="true" />
-                </button>
+        <header className="p-5 md:p-[40px_50px_40px_50px] ] [ h-162.5 w-screen xl:min-h-350 ] [ bg-[url(assets/images/mobile/image-hero.jpg)] bg-center bg-cover bg-no-repeat md:bg-[url(assets/images/desktop/image-hero.jpg)] ] select-none">
+            <MobileHeader 
+                isHeaderHidden={isHeaderHidden}
+                setIsHeaderHidden={setIsHeaderHidden}
+                isMenuHidden={isMenuHidden}
+                setIsMenuHidden={setIsMenuHidden}
+                heading={heading}
+            />
 
-                <section className="text-white max-xl:hidden xl:inline-flex gap-10 xl:text-xl">
-                    <a href="#" className="transition delay-50 duration-150 ease-in-out hover:scale-120 hover:border-b active:scale-120 active:border-b">About</a>
-                    <a href="#" className="transition delay-50 duration-150 ease-in-out hover:scale-120 hover:border-b active:scale-120 active:border-b">Careers</a>
-                    <a href="#" className="transition delay-50 duration-150 ease-in-out hover:scale-120 hover:border-b active:scale-120 active:border-b">Events</a>
-                    <a href="#" className="transition delay-50 duration-150 ease-in-out hover:scale-120 hover:border-b active:scale-120 active:border-b">Products</a>
-                    <a href="#" className="transition delay-50 duration-150 ease-in-out hover:scale-120 hover:border-b active:scale-120 active:border-b">Support</a>
-                </section>
-            </section>
-
-            <h1 className="[ self-start ] [ uppercase text-5xl md:text-7xl xl:text-9xl text-left text-white tracking-widest font-light ] [ p-5 ] [ border-2 border-white ] [ min-h-21.25 md:w-150 xl:w-300 xl:min-h-45 ] animate-fade-in-textBox" hidden={isHeaderHidden} aria-hidden={isHeaderHidden}>
-                {heading}
-            </h1>
-
-            <section id="HeaderMenu" className="grid grid-cols 1 auto-rows-min gap-35 m-[-20px] md:m-[-40px_-50px_-40px_-50px] md:p-[40px_50px_40px_50px] p-10 bg-black text-white animate-fade-in-dropDownMenu" hidden={isMenuHidden} aria-hidden={isMenuHidden} aria-expanded={!(isMenuHidden)}>
-                <div className="flex justify-between self-start">
-                    <p className="text-3xl font-bold">loopstudios</p>
-                    <button onClick={() => handleMenu(setIsHeaderHidden, setIsMenuHidden, isHeaderHidden, isMenuHidden)} 
-                            className="hover:cursor-pointer transition delay-50 duration-150 ease-in-out hover:scale-120 active:scale-120" aria-label="close-menu">
-                        <img src={closeIcon} className="pointer-events-none" aria-hidden="true" aria-controls="HeaderMenu"/>
-                    </button>
-                </div>
-
-                <nav className="text-3xl/15 text-left font-light uppercase select-none">
-                    <ul>
-                        <li className="transition delay-50 duration-150 ease-in-out hover:scale-102 hover:text-purple-500 active:scale-102 active:text-purple-500 animate-fade-in-about"><a href="#">About</a></li>
-                        <li className="transition delay-50 duration-150 ease-in-out hover:scale-102 hover:text-purple-500 active:scale-102 active:text-purple-500 animate-fade-in-careers"><a href="#">Careers</a></li>
-                        <li className="transition delay-50 duration-150 ease-in-out hover:scale-102 hover:text-purple-500 active:scale-102 active:text-purple-500 animate-fade-in-events"><a href="#">Events</a></li>
-                        <li className="transition delay-50 duration-150 ease-in-out hover:scale-102 hover:text-purple-500 active:scale-102 active:text-purple-500 animate-fade-in-products"><a href="#">Products</a></li>
-                        <li className="transition delay-50 duration-150 ease-in-out hover:scale-102 hover:text-purple-500 active:scale-102 active:text-purple-500 animate-fade-in-support"><a href="#">Support</a></li>
-                    </ul>
-                </nav>
-            </section>
+            <DesktopHeader
+                heading={heading}
+            />
         </header>
     );
 }
